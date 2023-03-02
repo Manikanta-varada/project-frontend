cde652095da68298e1d11fc7aa2c9fafa9e25c22
unknown <arjunvarada26@gmail.com>
2023-03-02 12:06:08 +0530

    deployed links

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -23,7 +23,7 @@ export default class CreateExercise extends Component {
   }
 
   componentDidMount() {
-    axios.get('http://localhost:5000/users/')
+    axios.get('https://backend-c649.onrender.com/users/')
       .then(response => {
         if (response.data.length > 0) {
           this.setState({
@@ -74,7 +74,7 @@ export default class CreateExercise extends Component {
 
     console.log(exercise);
 
-    axios.post('http://localhost:5000/exercises/add', exercise)
+    axios.post('https://backend-c649.onrender.com/add', exercise)
       .then(res => console.log(res.data));
 
     window.location = '/';
--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -28,7 +28,7 @@ export default class CreateUser extends Component {
 
     console.log(user);
 
-    axios.post('http://localhost:5000/users/add', user)
+    axios.post('https://backend-c649.onrender.com/users/add', user)
       .then(res => console.log(res.data));
 
     this.setState({
